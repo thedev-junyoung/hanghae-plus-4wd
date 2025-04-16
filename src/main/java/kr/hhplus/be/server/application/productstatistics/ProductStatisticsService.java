7641243f61ef22cefb2e64acdd8f1233725e4270
thedev-junyoung <thedev.junyoung@gmail.com>
2025-04-16 16:39:16 +0900

    feat: 레파지토리 구현체 1차 완료

--- src/main/java/kr/hhplus/be/server/application/productstatistics/ProductStatisticsService.java
+++ src/main/java/kr/hhplus/be/server/application/productstatistics/ProductStatisticsService.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.Collection;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
--- src/test/java/kr/hhplus/be/server/application/product/ProductFacadeTest.java
+++ src/test/java/kr/hhplus/be/server/application/product/ProductFacadeTest.java
@@ -38,7 +38,7 @@ class ProductFacadeTest {
         when(statisticsUseCase.getTopSellingProducts(criteria))
                 .thenReturn(List.of(info));
 
-        Product mockProduct = mockProduct(139000L);
+        Product mockProduct = mockProduct();
         when(productUseCase.findProduct(1L)).thenReturn(mockProduct);
 
         // when
@@ -56,11 +56,11 @@ class ProductFacadeTest {
         verify(productUseCase).findProduct(1L);
     }
 
-    private Product mockProduct(long price) {
+    private Product mockProduct() {
         Product product = mock(Product.class);
         when(product.getId()).thenReturn(1L);
         when(product.getName()).thenReturn("NIKE DUNK");
-        when(product.getPrice()).thenReturn(price);
+        when(product.getPrice()).thenReturn(139000L);
         when(product.getReleaseDate()).thenReturn(LocalDate.now());
         when(product.getCreatedAt()).thenReturn(LocalDateTime.now());
         when(product.getUpdatedAt()).thenReturn(LocalDateTime.now());
